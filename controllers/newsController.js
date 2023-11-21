f8973f403c99235c36ca2c43640114a21245f7e6
irubyeye <irubyeye@gmail.com>
2023-11-21 18:28:28 +0200

    FEATURE: Add global error handling

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,46 +1,42 @@
-const News = require('../models/newsModel');
+const News = require("../models/newsModel");
+const catchAsync = require("../utils/cathAsync");
+const AppError = require("../utils/appError");
 
-exports.createNews = async (req, res) => {
-  try {
-    const news = await News.create(req.body);
-    res.status(201).json(news);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+exports.createNews = catchAsync(async (req, res) => {
+  const news = await News.create(req.body);
+  res.status(201).json(news);
+});
 
 exports.getAllNews = async (req, res) => {
-  try {
-    const news = await News.find();
-    res.status(200).json(news);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  const news = await News.find();
+  res.status(200).json(news);
 };
 
-exports.getNewsById = async (req, res) => {
-  try {
-    const news = await News.findById(req.params.id);
-    res.status(200).json(news);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+exports.getNewsById = catchAsync(async (req, res, next) => {
+  const news = await News.findById(req.params.id);
+  if (!news) return next(new AppError("No news with such id", 404));
 
-exports.updateNews = async (req, res) => {
-  try {
-    const news = await News.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.status(200).json(news);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+  res.status(200).json(news);
+});
 
-exports.deleteNews = async (req, res) => {
-  try {
-    await News.findByIdAndDelete(req.params.id);
-    res.status(204).end();
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+exports.updateNews = catchAsync(async (req, res, next) => {
+  const news = await News.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!news) return next(new AppError("No news with such id", 404));
+
+  res.status(200).json(news);
+});
+
+exports.deleteNews = catchAsync(async (req, res, next) => {
+  const news = await News.findByIdAndDelete(req.params.id);
+
+  if (!news) return next(new AppError("No news with such id", 404));
+
+  res.status(204).json({
+    status: "deleted",
+    data: null,
+  });
+});
