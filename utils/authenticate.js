9bb3a8825a065ffa1b1667f79eb3d010c51cc7eb
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2024-03-30 19:37:08 +0200

    fix auth

--- utils/authenticate.js
+++ utils/authenticate.js
@@ -8,14 +8,13 @@ dotenv.config();
 const { ACCESS_SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
-  if (req.headers) {
-    const { authorization = "" } = req.headers;
-    const [bearer, token] = authorization.split(" ");
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
 
-    if (bearer !== "Bearer") {
-      req.myError = 401;
-      next();
-    }
+  if (bearer !== "Bearer") {
+    req.myError = 401;
+    next();
+  } else {
     try {
       const { id } = jwt.verify(token, ACCESS_SECRET_KEY);
       const user = await User.findById(id);
@@ -23,15 +22,14 @@ const authenticate = async (req, res, next) => {
         req.myError = 401;
 
         next();
+      } else {
+        req.user = user;
+        next();
       }
-      req.user = user;
-      next();
     } catch (error) {
       req.myError = 401;
       next();
     }
-  } else {
-    next();
   }
 };
 
